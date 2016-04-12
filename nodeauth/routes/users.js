1da042eeaab63071c4859cfeb096847591a4c2a7
m00ntasir <moontasir.dev@outlook.com>
2016-04-11 23:16:14 -0400

    Login done

--- nodeauth/routes/users.js
+++ nodeauth/routes/users.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var multer = require('multer');
 var upload = multer({ dest: './uploads' });
+var User =  require('../models/user');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -15,13 +19,58 @@ router.get('/login', function(req, res, next) {
   res.render('login', {title:'Login'});
 });
 
+router.post('/login',
+  passport.authenticate('local', {
+      failureRedirect: '/users/login',
+      failureFlash: 'Invalid User Name or Password'
+    }),
+  function(req, res) {
+    req.flash('success', 'You are logged in');
+    res.redirect('/');
+  }
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(function (username, password, done) {
+    User.getUserByUsername(username, function(err, user){
+        if(err) {
+            throw err;
+        }
+        
+        if(!user) {
+            return done(null, false, {message:'Unknown User.'})
+        }
+        
+        User.comparePassword(password, user.password, function(err, isMatch) {
+            if(err) {
+                return done(err);                 
+            }
+            
+            if(isMatch) {
+                return done(null, user);
+            } else {
+                return done(null, false, {message: 'Invalid Username or password'});
+            }
+        });
+    });
+}));
+
 router.post('/register',upload.single('profileimage'), function(req, res, next) {
   var name = req.body.name;
   var email = req.body.email;
   var username = req.body.username;
-  var pasoword = req.body.password;
+  var password = req.body.password;
   var confirmpassword = req.body.confirmpassword;  
-   var profileImage;
+  var profileImage;
   if(req.file) {
     profileImage = req.file.fileName;
   } else {
@@ -44,7 +93,22 @@ router.post('/register',upload.single('profileimage'), function(req, res, next) 
         errors: errors
     });   
   } else {
-    console.log('No Errors');
+  	var newUser = new User({
+      name: name,
+      email: email,
+      username: username,
+      password: password,
+      profileimage: profileImage
+    });
+
+    User.createUser(newUser, function(err, user){
+      if(err) throw err;
+      console.log(user);
+    });
+
+    req.flash('success', 'You are now registered and can login');
+    res.location('/');
+    res.redirect('/');
   }
   
 });
