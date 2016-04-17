b979e84e429b4e2e7345ff7bcf88064faa374068
m00ntasir <moontasir.dev@outlook.com>
2016-04-17 11:42:54 -0400

    Update layout

--- nodeblog/app.js
+++ nodeblog/app.js
@@ -1,9 +1,18 @@
-var express = require('express');
+    var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' });
+var moment = require('moment');
+var expressValidation = require('express-session');
+var flash = require('connect-flash');
+var expressValidator = require('express-validator');
+var mongo = require('mongodb');
+var db = require('monk')('localhost/nodeblog');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -21,6 +30,44 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle Session
+app.use(session({
+  secret:'secret',
+  saveUninitialized: true,
+  resave: true
+}));
+
+//Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+//Connect flash
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+//Setting DB
+app.use(function(req, res, next) {
+    req.db = db;
+    next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
