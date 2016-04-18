164a3dc8074474487aae4d05a756d0020fd0aafd
m00ntasir <moontasir.dev@outlook.com>
2016-04-18 18:21:20 -0400

    ckeditor

--- nodeblog/routes/posts.js
+++ nodeblog/routes/posts.js
@@ -7,8 +7,13 @@ var db = require('monk')('localhost/nodeblog');
 
 /* GET posts listing. */
 router.get('/add', function(req, res, next) {
-  res.render('addpost', {
-    title: 'Add post'
+  var categories = db.get('categories');
+  
+  categories.find({}, {}, function (err, categories) {
+    res.render('addpost', {
+      "title": "Add post",
+      "categories":categories     
+    });
   });
 });
 
@@ -18,6 +23,40 @@ router.post('/add', upload.single('mainimage'), function(req, res, next) {
   var body =  req.body.body;
   var author =  req.body.author;
   var date =  new Date();
+  var mainimage = 'No Image File';
+  if(req.file) {
+    mainimage = req.file.name;
+  }
+  
+  req.checkBody('title','Title filed is required').notEmpty();
+  req.checkBody('body','Body filed is required').notEmpty();
+  
+  var errors = req.validationErrors();
+  
+  if(errors) {
+    res.render('addpost', {
+      "errors": errors
+    });
+  } else {
+    var posts =  db.get('posts');
+		posts.insert({
+			"title": title,
+			"body": body,
+			"category": category,
+			"date": date,
+			"author": author,
+			"mainimage": mainimage
+		}, function(err, post){
+			if(err){
+				res.send(err);
+			} else {
+				req.flash('success','Post Added');
+				res.location('/');
+				res.redirect('/');
+			}
+		});
+  }
+  
   console.log(title);
 });
 
